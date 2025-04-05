a5ddad8fc9a2e20e6d4cf3d11d2ae5d54a89e157
Facundo Hernandez <facundohernandez@outlook.com>
2025-04-05 15:08:21 -0300

    redirect uri

--- src/authConfig.js
+++ src/authConfig.js
@@ -14,8 +14,7 @@ export const msalConfig = {
     auth: {
         clientId: "d424e7b6-06b6-452d-9a03-b4ea8c01c0c0",
         authority: "https://login.microsoftonline.com/37cd273a-1cec-4aae-a297-41480ea54f8d",
-        redirectUri: "horariosmda.vercel.app",
-
+        redirectUri: "https://horariosmda.vercel.app",
 
     },
     cache: {
