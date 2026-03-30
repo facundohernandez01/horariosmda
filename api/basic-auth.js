8ba6c7052fb4b485f30826f58721cb7220ee93d9
facundohernandez01 <facundohernandez@outlook.com>
2026-03-30 11:45:52 -0300

    proeccion de acceso nivel .env

---
+++ api/basic-auth.js
@@ -0,0 +1,27 @@
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req) {
+  const auth = req.headers.get('authorization');
+
+  const USER = process.env.BASIC_AUTH_USER;
+  const PASS = process.env.BASIC_AUTH_PASS;
+
+  if (auth) {
+    const encoded = auth.split(' ')[1];
+    const decoded = atob(encoded);
+    const [user, pass] = decoded.split(':');
+
+    if (user === USER && pass === PASS) {
+      return fetch(req); // deja pasar al sitio
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Protected Area"',
+    },
+  });
+}
